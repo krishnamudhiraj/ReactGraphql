4511513fbaf7a30184e9bf6b761ddf6381ca2c05
Krishna MS <krishna@Krishnas-MacBook-Air-2.local>
2018-11-07 13:36:10 +0530

    frontend and backend

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const mutations = {};
+const Mutation = {
+	async createItem(parent, args, ctx, info) {
+		// TODO check if they are logged in
+		// console.log(ctx, '******');
+		const Item = await ctx.db.mutation.createItem(
+			{
+				data: {
+					...args,
+				},
+			},
+			info
+		);
+		return Item;
+	},
+};
 
-module.exports = mutations;
+module.exports = Mutation;
